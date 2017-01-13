eea3989d98aecf8b097377b1dd067644aba2603f
[chengxinping] <[972649353@qq.com]>
2017-01-13 11:41:24 +0800

    完成项目框架

---
+++ app/src/main/java/com/cxp/jokes/fragment/GifFragment.java
@@ -0,0 +1,23 @@
+package com.cxp.jokes.fragment;
+
+import android.view.View;
+
+import com.cxp.jokes.R;
+import com.cxp.jokes.base.BaseFragment;
+
+/**
+ * Created by cxp on 17-1-13.
+ */
+
+public class GifFragment extends BaseFragment {
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public View initView() {
+        View view = View.inflate(mActivity, R.layout.fragment_content, null);
+        return view;
+    }
+}
